3e3db6a0c5f678bb6351f147c594a193ceed6141
tabatay <sharp_eyes.0322@icloud.com>
2019-01-01 18:32:51 +0900

    twitter API 承認待ちになってしまった

--- top-tweets/app.py
+++ top-tweets/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods = ["GET", "POST"]) # hoge.com/にアクセスした時に実行される関数
 def index():
     if request.method == 'POST':
-        user_id = request.form['user_id'] # formのname = "user_id"を取得
-        return render_template('index.html', user_id = user_id) # index.htmlを呼び出す
+        show_text = request.form['show_text'] # formのname = "show_text"を取得
+        return render_template('index.html', show_text = show_text) # index.htmlを呼び出す
     else:
         return render_template('index.html')
 app.run(host="localhost")
